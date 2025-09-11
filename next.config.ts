d1ea56fe311a1672a54ed2c38638c609f1716b59
Alex Kimbrell <alexkimbrell31@gmail.com>
2025-09-10 22:26:36 -0400

    Trying Again to fix next.config.ts file, adding BasePath

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+  basePath: '/movie-web-scraper',
   images: {
     unoptimized: true,
   },

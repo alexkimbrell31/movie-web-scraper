0a307fcb0f74092be3d8af8fd296325259bd6c8e
Alex Kimbrell <alexkimbrell31@gmail.com>
2025-09-10 22:57:43 -0400

    Re-Adding Large CSV File

--- src/app/MainPage.tsx
+++ src/app/MainPage.tsx
@@ -1,16 +1,12 @@
 'use client'
 import Header from './components/Header'
-// import SearchBar from "./components/SearchBar";
+import SearchBar from "./components/SearchBar";
 
 export const MainPage = () => {
     return (
       <div>
         <Header />
-        <hr></hr>
-        <h1>
-          FREEDOM!!!!!
-        </h1>
-        {/* <SearchBar /> */}
+        <SearchBar />
       </div>
     );
   }

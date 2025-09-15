ac26e285c593abe72fab6d6c9769421ad993b067
Alex Kimbrell <alexkimbrell31@gmail.com>
2025-09-14 23:04:49 -0400

    Removing old comments from interview prep

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,48 +1,4 @@
-// import Image from "next/image";
 import MainPage from "./MainPage";
-// import styles from "./page.module.css";
-
-// REACT STUDY
-// 1. Create your own custom hook
-// 2. useState, useEffect, useContext, useRef, useMemo, useCallback (how each works, and examples of how to use them)
-// 3. How to handle side effects and data fetching with useEffect
-// 4. Component-level state vs Context API vs Redux (why one over the other? When to use each)
-// 5. Fundamentals of Redux
-// 6. Functional vs Class Components (when to use which - functional are ususally preferred (apparently))
-// 7. JSX (basics of how it works)
-// 8. Props vs State (How they're different, lifting state up)
-// 9. Basics of the Virtual DOM (how does it work)
-// 10. Component Lifecycle (hooks for functional components)
-// 11. How indexs/keys work in React
-// 12. Controlled vs Un-controlled components 
-// 13. Context API
-// 14. Storing data in browser (localStorage vs windowStorage - when to use each)
-// 15. General Best Practices for React projects
-// 16. Simple Unit Testing w/ Jest
-// 17. High-level understanding of webpack
-// 18. How routing works in React (different pages for Single Page App)
-// 19. React vs other web frameworks (Vue, Angular)?
-// 20. Component Lifecyle for React (common hooks and when they fire)
-
-
-// JavaScript/TypeScript STUDY
-// 1. Closures?
-// 2. Async/Await
-// 3. Promises
-// 4. Array methods (map, filter, forEach, reduce)
-// 5. Why TypeScript over JS?
-// 6. 'any' type and when to use it
-// 7. Arrow functions vs regular functions (what's the difference?)
-
-
-
-
-// 1. custom Hook
-// 2. useCallback and useMemo examples
-// 3. refresh on redux
-// 4. Glance at Jest
-
-
 
 export default function Home() {
   return (
